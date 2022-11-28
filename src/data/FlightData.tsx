eaa2263966bb9d106ce3a2a6b3a6c543536f4c62
Zilu Wang <32582981+ziluwang829@users.noreply.github.com>
2022-11-28 00:32:35 -0500

    FlightData added

---
+++ src/data/FlightData.tsx
@@ -0,0 +1,60 @@
+import React, {useEffect} from "react";
+import {makeReq} from "./FlightDataManager";
+
+export type FlightsByAircraft = {
+  icao24: string;
+  firstSeen: number;
+  estDepartureAirport: string | null;
+  lastSeen: number;
+  estArrivalAirport: string | null;
+  callsign: string | null;
+  estDepartureAirportHorizDistance: number;
+  estDepartureAirportVertDistance: number;
+  estArrivalAirportHorizDistance: number;
+  estArrivalAirportVertDistance: number;
+  departureAirportCandidatesCount: number;
+  arrivalAirportCandidatesCount: number;
+}
+
+async function getFlightsByAircraft(ICAO: string, rewind: number = 86400): Promise<FlightsByAircraft[] | null> {
+  const flights = await makeReq("/flights/aircraft", {'icao24': ICAO, 'begin': Date.now() - rewind, 'end': Date.now()});
+  if (!flights) return null;
+  return (flights.states as any[]).map<FlightsByAircraft>((item: any) => ({
+    icao24: item[0],
+    firstSeen: item[1],
+    estDepartureAirport: item[2],
+    lastSeen: item[3],
+    estArrivalAirport: item[4],
+    callsign: item[5],
+    estDepartureAirportHorizDistance: item[6],
+    estDepartureAirportVertDistance: item[7],
+    estArrivalAirportHorizDistance: item[8],
+    estArrivalAirportVertDistance: item[9],
+    departureAirportCandidatesCount: item[10],
+    arrivalAirportCandidatesCount: item[11]
+  }));
+}
+
+export type FlightManagerProps = {
+  setFlight: (flight: FlightsByAircraft[]) => void;
+  ICAO: string;
+};
+
+const FlightManager: React.ComponentType<FlightManagerProps> = (p) => {
+  useEffect(() => {
+    const allFlights = getFlightsByAircraft(p.ICAO);
+    allFlights.then((res) => {
+      if (res == null) {
+        alert("Cannot get flight data.");
+        return;
+      }
+      console.log("All flights has been acquired.");
+      console.dir(res);
+      p.setFlight(res);
+    });
+  }, []);
+
+  return (<></>);
+};
+
+export default FlightManager; 
