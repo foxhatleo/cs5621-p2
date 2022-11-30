863fb28fa1dc0b8351485142dfa53273b6d69edf
Zilu Wang <zw427@cornell.edu>
2022-11-29 23:17:50 -0500

    a
    a

--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -99,6 +99,72 @@ const Earth: React.ComponentType<EarthProps> = (p) => {
     p.setSelected(ind);
   };
 
+  const randomData = {
+    source_lat: 40.712778,
+    source_lng: -74.006111,
+    source_alt: 0,
+    target_lat: 40.712778, 
+    target_lng: -74.006111-10,
+    target_alt: 0.05,
+    points : 50,
+  };
+
+  function arc(d:any) {
+    const s = myGlobe?.current!.getCoords(d.source_lat, d.source_lng, d.source_alt)
+    const source = new THREE.Vector3(s.x, s.y, s.z)
+    const t = myGlobe?.current!.getCoords(d.target_lat, d.target_lng, d.target_alt)
+    const target = new THREE.Vector3(t.x, t.y, t.z)
+    const globe = new THREE.Vector3(0, 0, 0)
+    const v = target.clone().sub(source)
+    console.log("source")
+    console.log(source)
+    console.log("target")
+    console.log(target)
+    const normal = v.clone()
+    console.log("normal")
+    console.log(normal)
+    const midpoint = v.clone().multiplyScalar(0.5).add(source)
+    const opposite_source = globe.clone().add(globe.clone().add(source.clone().multiplyScalar(-10)))
+    const old_r = source.clone().add(
+      (
+        opposite_source.clone().sub(source).multiplyScalar
+        (
+          normal.dot(midpoint.clone().sub(source)) /
+          normal.dot(opposite_source.clone().sub(source))
+        )
+      )
+    )
+    console.log(old_r)
+    const r = old_r // midpoint.clone().add(midpoint.clone().sub(old_r))
+    const ra = r.distanceTo(source)
+    console.log(r)
+    console.log(ra)
+
+    var p = new Array(d.points).fill(null).map((_, i) => i + 1);
+    const points = p.map((x, _) => {
+      const temp = v.clone().multiplyScalar(x / d.points).add(source)
+      return r.clone().add(temp.sub(r).normalize().multiplyScalar(ra))
+    })
+    var points_pairs = points.map((x, i) => {
+      if (i + 1 === points.length) {
+        return [x, x]
+      }
+      return [x, points[i + 1]]
+    })
+    points_pairs.pop()
+    return points_pairs
+  }
+  const [arcData, setArcData] = useState<null | THREE.Vector3[][]>(null);
+  useEffect(() => {
+    setArcData(arc(randomData));
+  }, []);
+
+  const arcDrawer = useCallback((v: any) => {
+    const geometry = new THREE.BufferGeometry().setFromPoints(v);
+    const material = new THREE.LineBasicMaterial({color: 'green'});
+    return new THREE.Line(geometry, material)
+  }, []);
+
   return (
     <div>
       <Globe width={width}
@@ -111,7 +177,15 @@ const Earth: React.ComponentType<EarthProps> = (p) => {
              objectLng="lng"
              objectsData={objectsData}
              objectThreeObject={satObject}
-             onObjectClick={onSelect} />
+             onObjectClick={onSelect}
+             customLayerData={arcData ? arcData : []} 
+             customThreeObject={arcDrawer}
+             customThreeObjectUpdate={(obj, d:any) => 
+             {
+              const geometry = new THREE.BufferGeometry().setFromPoints(d);
+              Object.assign(obj.position, geometry.attributes.position);
+             }}
+            />
     </div>
   )
 };
